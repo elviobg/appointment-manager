056700848e9819e7af071fe2e075c4eeaf4307d0
Elvio <elviobg@gmail.com>
2021-03-06 19:47:22 -0300

    organize folder structure to db connection helper

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const db = require('./../db')
+const db = require('../database/databaseConnection')
 
 /* GET users listing. */
 router.get('/test', function (req, res, next) {
