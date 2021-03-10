340a4519dcba0305b1b1b9b9d3900125c8538721
Elvio <elviobg@gmail.com>
2021-03-10 17:53:39 -0300

    Adds a simple logout

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -8,12 +8,12 @@ module.exports.login = async function (req, res) {
   db.users.findOne({ where: { login: login } })
     .then(async function (user) {
       if (!user) {
-        return res.status(404).send(JSON.stringify('User not found'))
+        return res.status(404).send({ auth: false, message: 'User not found' })
       }
       if (await bcrypt.compare(password, user.password)) {
-        res.status(200).send({auth: true, token:JWT.generateJWT(user['uuid'])})
+        res.status(200).send({ auth: true, token: JWT.generateJWT(user.uuid) })
       } else {
-        res.status(401).send(JSON.stringify('Invalid login and/or password'))
+        res.status(401).send({ auth: false, message: 'Invalid login and/or password' })
       }
     })
     .catch(err => {
@@ -22,5 +22,5 @@ module.exports.login = async function (req, res) {
 }
 
 module.exports.logout = function (req, res) {
-  res.status(200).send(JSON.stringify('logout'))
+  res.status(200).send({ auth: false, token: null })
 }
