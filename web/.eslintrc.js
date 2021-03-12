0cb8de69b931673b075d15fb23f0d6c5e18ebba6
Elvio <elviobg@gmail.com>
2021-03-11 21:33:33 -0300

    Add eslint and apply same style guide that api

---
+++ web/.eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    jest: true
+  },
+  extends: [
+    'standard',
+    'react-app',
+    'react-app/jest'
+  ],
+  parserOptions: {
+    ecmaVersion: 12
+  },
+  rules: {
+  }
+
+}
