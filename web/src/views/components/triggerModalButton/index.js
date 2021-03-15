794432220a71d6b53fa0624e033f54a8dae8fd83
Elvio <elviobg@gmail.com>
2021-03-14 21:10:38 -0300

    Adds styel to create new pacient/appointment button

--- web/src/views/components/triggerModalButton/index.js
+++ web/src/views/components/triggerModalButton/index.js
@@ -1,14 +1,26 @@
 import React from 'react'
+import Button from '@material-ui/core/Button'
+import { makeStyles } from '@material-ui/core/styles'
+
+const buttonStyles = makeStyles((theme) => ({
+  submit: {
+    margin: theme.spacing(3, 0, 2)
+  }
+}))
 
 const Trigger = ({ triggerText, buttonRef, showModal }) => {
+  const classes = buttonStyles()
   return (
-    <button
-      className="btn btn-lg btn-danger center modal-button"
+    <Button
+      fullWidth
+      variant="contained"
+      color="primary"
+      className={classes.submit}
       ref={buttonRef}
       onClick={showModal}
     >
       {triggerText}
-    </button>
+    </Button>
   )
 }
 export default Trigger
